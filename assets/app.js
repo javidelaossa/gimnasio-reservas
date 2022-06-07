abc7e04e423ef47b4ff450c15e18e45913f53775
javier <javierdelaossapuerto@gmail.com>
2022-06-07 11:52:45 +0200

    Bootstrap activo

---
+++ assets/app.js
@@ -0,0 +1,18 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.scss in this case)
+import './styles/app.scss';
+import $ from 'jquery';
+import './bootstrap';
+// require ('bootstrap');
+// const $ = require('jquery');
+// start the Stimulus application
+// console.log('hola');
+
+// const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+// const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
